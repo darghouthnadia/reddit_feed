91dcc166c9e1ac16cf6c3bcd45ffdda978b5da67
Darghouth Nadia <darghouthnadia@gmail.com>
2021-10-24 13:46:47 +0200

    Added logo and routing to home

--- reddit-feed/src/app/header/header.component.ts
+++ reddit-feed/src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public redditLogo = "/assets/img/reddit-logo.png"
   constructor() { }
 
   ngOnInit(): void {
